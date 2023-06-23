e58b65e202e9ecbd017216c68a4bdba3b540d95d
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-06-23 16:58:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,56 +1,26 @@
-//your JS code here.
+document.addEventListener('DOMContentLoaded', () => {
+  const questions = document.querySelectorAll('.question');
+  const submitButton = document.getElementById('submit');
+  const scoreDiv = document.getElementById('score');
 
-// Do not change code below this line
-// This code will just display the questions to the screen
-const questions = [
-  {
-    question: "What is the capital of France?",
-    choices: ["Paris", "London", "Berlin", "Madrid"],
-    answer: "Paris",
-  },
-  {
-    question: "What is the highest mountain in the world?",
-    choices: ["Everest", "Kilimanjaro", "Denali", "Matterhorn"],
-    answer: "Everest",
-  },
-  {
-    question: "What is the largest country by area?",
-    choices: ["Russia", "China", "Canada", "United States"],
-    answer: "Russia",
-  },
-  {
-    question: "Which is the largest planet in our solar system?",
-    choices: ["Earth", "Jupiter", "Mars"],
-    answer: "Jupiter",
-  },
-  {
-    question: "What is the capital of Canada?",
-    choices: ["Toronto", "Montreal", "Vancouver", "Ottawa"],
-    answer: "Ottawa",
-  },
-];
+  // Load progress from session storage
+  const progress = JSON.parse(sessionStorage.getItem('progress')) || {};
 
-// Display the quiz questions and choices
-function renderQuestions() {
-  for (let i = 0; i < questions.length; i++) {
-    const question = questions[i];
-    const questionElement = document.createElement("div");
-    const questionText = document.createTextNode(question.question);
-    questionElement.appendChild(questionText);
-    for (let j = 0; j < question.choices.length; j++) {
-      const choice = question.choices[j];
-      const choiceElement = document.createElement("input");
-      choiceElement.setAttribute("type", "radio");
-      choiceElement.setAttribute("name", `question-${i}`);
-      choiceElement.setAttribute("value", choice);
-      if (userAnswers[i] === choice) {
-        choiceElement.setAttribute("checked", true);
-      }
-      const choiceText = document.createTextNode(choice);
-      questionElement.appendChild(choiceElement);
-      questionElement.appendChild(choiceText);
+  // Restore selected options
+  for (const questionId in progress) {
+    const question = document.getElementById(questionId);
+    const selectedOption = progress[questionId];
+    const optionElement = question.querySelector(`input[value="${selectedOption}"]`);
+    if (optionElement) {
+      optionElement.checked = true;
     }
-    questionsElement.appendChild(questionElement);
   }
-}
-renderQuestions();
+
+  // Event listener for option selection
+  questions.forEach((question) => {
+    const options = question.querySelectorAll('input[type="radio"]');
+    options.forEach((option) => {
+      option.addEventListener('change', () => {
+        const questionId = question.id;
+        const selectedOption = question.querySelector('input[type="radio"]:checked').value;
+        progress[questionId] = selected
